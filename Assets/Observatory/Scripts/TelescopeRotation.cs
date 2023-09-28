1c599a367e8140662b998ba7fd4941e444e5bcc8
Antonia Cecilia <126273091+antoniacecilia@users.noreply.github.com>
2023-09-28 14:58:30 +0200

    Telescope rotation
    Added: New Telescope FBX with correct pivot, Audio files, Rotation script

---
+++ Assets/Observatory/Scripts/TelescopeRotation.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class TelescopeRotation : MonoBehaviour
+{
+    [Tooltip("Speed of the rotation in degrees per second")]
+    public float rotationSpeed = 30f;
+
+    [Tooltip("Maximum angle of rotation")]
+    public float maxRotationAngle = 20f;
+
+    private float currentRotationAngle = 0f;
+
+    private AudioSource audioSource;
+
+    private bool hasAudioPlayed = false;
+
+    void Start()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (currentRotationAngle < maxRotationAngle)
+        {
+            // Play audio when rotation starts, but only once
+            if (!hasAudioPlayed)
+            {
+                audioSource.Play();
+                hasAudioPlayed = true;
+            }
+
+            float rotationThisFrame = rotationSpeed * Time.deltaTime;
+            float possibleRotation = currentRotationAngle + rotationThisFrame;
+
+            if (possibleRotation > maxRotationAngle)
+            {
+                rotationThisFrame = maxRotationAngle - currentRotationAngle;
+            }
+
+            transform.Rotate(rotationThisFrame, 0, 0, Space.Self);
+            currentRotationAngle += rotationThisFrame;
+        }
+    }
+}
