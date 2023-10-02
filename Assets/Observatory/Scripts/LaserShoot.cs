d3a330ec199377f55edfeab5b2f2b40e38f59c27
Antonia Cecilia <126273091+antoniacecilia@users.noreply.github.com>
2023-10-02 11:59:47 +0200

    Laser Observatory
    Second try implementing Laser script

--- Assets/Observatory/Scripts/LaserShoot.cs
+++ Assets/Observatory/Scripts/LaserShoot.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LaserShoot : MonoBehaviour
 {
     public float laserLength = 100f;
+    public float laserDuration = 0.02f;  // You can adjust this in the Inspector
+    public UnityEvent OnLaserShootComplete;  // UnityEvent to be invoked when the laser shoot is complete
+
     private LineRenderer lineRenderer;
 
     void Start()
@@ -20,8 +24,10 @@ public class LaserShoot : MonoBehaviour
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, laserEnd);
 
-        yield return new WaitForSeconds(0.02f);
+        yield return new WaitForSeconds(laserDuration);
 
         lineRenderer.enabled = false;
+
+        OnLaserShootComplete?.Invoke();  // Invoke the UnityEvent
     }
 }
