7138b4202a47a84867f4e324448cd90f3f7f9dfe
Oliver <oliver.marback@hyperisland.se>
2023-10-04 16:49:39 +0200

    Script changes
    Script changes

--- Assets/Observatory/Scripts/ObservatoryRotate.cs
+++ Assets/Observatory/Scripts/ObservatoryRotate.cs
@@ -1,13 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class ObservatoryRotate : MonoBehaviour
 {
     public float rotationSpeed = 1.0f; // Speed of rotation
+    public Transform player; // Reference to the player's transform
 
     // Unity event to start rotation
-    public void StartRotation(float degrees)
+    public void StartRotation(float targetDegrees, Transform playerTransform)
     {
-        // Rotate the GameObject around its y-axis
-        transform.Rotate(new Vector3(0, degrees * rotationSpeed, 0));
+        // Set the player reference
+        player = playerTransform;
+
+        // Start a coroutine to gradually rotate the GameObject
+        StartCoroutine(RotateOverTime(targetDegrees));
+    }
+
+    // Coroutine to gradually rotate the GameObject
+    private IEnumerator RotateOverTime(float targetDegrees)
+    {
+        float currentRotation = 0f;
+
+        while (currentRotation < targetDegrees)
+        {
+            float rotationThisFrame = rotationSpeed * Time.deltaTime;
+            currentRotation += rotationThisFrame;
+
+            // Rotate the GameObject around its y-axis
+            transform.Rotate(new Vector3(0, rotationThisFrame, 0));
+
+            // Rotate the player along with the observatory
+            if (player != null)
+            {
+                player.Rotate(new Vector3(0, rotationThisFrame, 0));
+            }
+
+            yield return null;
+        }
     }
 }
