769b82a7c5d38380aea092c50664a9ab734ed050
Zuhaib001 <zuhaib.paracha@hyperisland.se>
2023-10-11 18:55:48 +0200

    Updated Point A, B  and C and Activated scene manager
    Updated Point A, B  and C and Activated scene manager

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,22 @@ public class PlayerController : MonoBehaviour
 {
     public Transform pointA;  // Set this to the transform of point A
     public Transform pointB;  // Set this to the transform of point B
+    public Transform pointC;  // Set this to the transform of point C
 
     private Transform playerTransform;
     private Vector3 startPosition;
     private Vector3 endPosition;
-    private float travelTime = 50f;  // Increase this value to slow down the movement further
-    private float maxHorizontalOffset = 1f;
+    private float travelTimeAB = 7f;  // Travel time from point A to B (7 seconds)
+    private float travelTimeBC = 35f;  // Travel time from point B to C (35 seconds)
     private float currentTime = 0f;
+
+    private enum MovementState
+    {
+        MovingToB,
+        MovingToC
+    }
+
+    private MovementState currentState = MovementState.MovingToB;
 
     private void Start()
     {
@@ -23,14 +32,35 @@ public class PlayerController : MonoBehaviour
     {
         currentTime += Time.deltaTime;
 
-        // Calculate the interpolation value (0 to 1) based on time
-        float t = Mathf.Clamp01(currentTime / travelTime);
+        if (currentState == MovementState.MovingToB)
+        {
+            if (currentTime < travelTimeAB)
+            {
+                // Calculate the interpolation value (0 to 1) based on time for segment AB
+                float t = Mathf.Clamp01(currentTime / travelTimeAB);
 
-        // Move the player smoothly from A to B
-        playerTransform.position = Vector3.Lerp(startPosition, endPosition, t);
+                // Move the player smoothly from A to B
+                playerTransform.position = Vector3.Lerp(startPosition, endPosition, t);
+            }
+            else
+            {
+                // Switch to segment BC
+                currentState = MovementState.MovingToC;
+                startPosition = pointB.position;
+                endPosition = pointC.position;
+                currentTime = 0f; // Reset time for the new segment
+            }
+        }
+        else if (currentState == MovementState.MovingToC)
+        {
+            if (currentTime < travelTimeBC)
+            {
+                // Calculate the interpolation value (0 to 1) based on time for segment BC
+                float t = Mathf.Clamp01(currentTime / travelTimeBC);
 
-        // Add limited left-to-right movement
-        float horizontalOffset = maxHorizontalOffset * Mathf.Sin(Time.time);
-        playerTransform.position += new Vector3(horizontalOffset, 0, 0);
+                // Move the player smoothly from B to C
+                playerTransform.position = Vector3.Lerp(startPosition, endPosition, t);
+            }
+        }
     }
 }
--- Assets/SceneChange.cs
+++ Assets/SceneChange.cs
@@ -3,18 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public string SceneToLoad;
+    public string sceneToLoad; // The name of the scene you want to load
 
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {        
-            SceneManager.LoadScene(SceneToLoad);
+        {
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
-
-
-
-
