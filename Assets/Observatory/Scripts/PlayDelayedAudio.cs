2319fc065348d48c7f032961bf543dc2bd728d72
OliverMfQ <126697401+MarferQQ@users.noreply.github.com>
2023-10-20 13:47:55 +0200

    SpaceScene + BlackHole Scene Transitions
    SpaceScene + BlackHole Scene Transitions

--- Assets/Observatory/Scripts/PlayDelayedAudio.cs
+++ Assets/Observatory/Scripts/PlayDelayedAudio.cs
@@ -33,4 +33,6 @@ public class PlayDelayedAudio : MonoBehaviour
         // Play the audio
         audioSource.Play();
     }
+
+
 }
--- Assets/Scripts/FadeOutScript.cs
+++ Assets/Scripts/FadeOutScript.cs
@@ -4,23 +4,29 @@ using BNG;
 
 public class FadeOutScript : MonoBehaviour
 {
-    public ScreenFader screenFader; 
-    public float delayInSeconds; 
+    public ScreenFader screenFader;
+    public float delayInSeconds;
     public float fadeOutTime;
+
+    //[SerializeField] 
+    //private Color fadeOutColor; 
 
     private void Start()
     {
+        // Set the fade-out color
+        //screenFader.FadeMaterial.color = fadeOutColor;
+
+        // Set the fade-out speed
         screenFader.FadeInSpeed = fadeOutTime;
+
         StartCoroutine(FadeOutAfterDelay());
     }
-
 
     private IEnumerator FadeOutAfterDelay()
     {
         yield return new WaitForSeconds(delayInSeconds);
 
-        
         screenFader.DoFadeIn();
-        
     }
+
 }
