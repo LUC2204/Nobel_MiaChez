7f01e5e448ccdd59eb1af575e5b2c55ebb460d8e
OliverMfQ <126697401+MarferQQ@users.noreply.github.com>
2023-10-20 22:58:13 +0200

    LAST CHANGES HERE WE GO
    LAST CHANGES HERE WE GO

--- Assets/Scripts/WhiteFadeout.cs
+++ Assets/Scripts/WhiteFadeout.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using UnityEngine;
 using BNG;
 
-public class FadeOutScript : MonoBehaviour
+public class WhiteFadeout : MonoBehaviour
 {
     public ScreenFader screenFader;
     public float delayInSeconds;
     public float fadeOutTime;
-    //public AudioFadeout _audioFadeout;
+    public AudioFadeout _audioFadeout;
 
 
-    //[SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSource;
 
     /// <summary>
-    /// /private Color fadeOutColor; 
+    private Color fadeOutColor; 
     /// </summary>
 
     private void Start()
     {
-        //_audioSource.volume = 1.0f;
+        _audioSource.volume = 1.0f;
         // Set the fade-out color
         //screenFader.FadeMaterial.color = fadeOutColor;
 
-        //screenFader.FadeColor = fadeOutColor;
+        screenFader.FadeColor = fadeOutColor;
 
         // Set the fade-out speed
         screenFader.FadeInSpeed = fadeOutTime;
@@ -33,10 +33,10 @@ public class FadeOutScript : MonoBehaviour
     private IEnumerator FadeOutAfterDelay()
     {
         yield return new WaitForSeconds(delayInSeconds);
-        
+
         screenFader.DoFadeIn();
-        //StartCoroutine(_audioFadeout.DoFadeOutAudio(_audioSource));
-        
+        StartCoroutine(_audioFadeout.DoFadeOutAudio(_audioSource));
+
     }
 
 }
